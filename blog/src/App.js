f8240d76362be389169f753dcb3f16497d2ae2c1
markogarovic <markogarovic1998@gmail.com>
2022-01-04 16:31:05 +0100

    Postavljanje blog aplikacije

---
+++ blog/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./Components/Card";
+
+function App() {
+  return (
+    <div>
+      <div className="row">
+        <Card></Card>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ blog/src/Routes/PostCreate.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostCreate = () => {
+  return <div></div>;
+};
+
+export default PostCreate;
