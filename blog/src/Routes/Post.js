f4b16a607d3e0329b12e4ed2ab04112453b76d33
markogarovic <markogarovic1998@gmail.com>
2022-01-05 19:58:22 +0100

    Kreirana forma / neophodno dodati logiku za slanje slike

--- blog/src/Routes/Post.js
+++ blog/src/Routes/Post.js
@@ -1,21 +1,23 @@
 import React from "react";
 
 export function Post(props) {
-console.log(props.location.aboutProps);
+  // console.log(props.location.aboutProps);
   return (
-    <div className="row">
-      <div className="col s12 m7">
-        <div className="card">
-          <div className="card-image">
-            <img src="images/sample-1.jpg" alt="img" />
-            <span className="card-title">Card Title</span>
-          </div>
-          <div className="card-content">
-            <p>
-              I am a very simple card. I am good at containing small bits of
-              information. I am convenient because I require little markup to
-              use effectively.
-            </p>
+    <div>
+      <div className="row">
+        <div className="col s12">
+          <div className="card">
+            <div className="card-image">
+              <img src="images/sample-1.jpg" alt="img" />
+              <span className="card-title">Card Title</span>
+            </div>
+            <div className="card-content">
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
           </div>
         </div>
       </div>
